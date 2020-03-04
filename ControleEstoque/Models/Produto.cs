edefa857921cf36647a329f6e2ed5498cf6672bf
lucas.soares <lucas.soares@NGS-321>
2020-03-04 17:56:19 -0300

    Ajustes de espaçamentos

--- ControleEstoque/Models/Produto.cs
+++ ControleEstoque/Models/Produto.cs
@@ -10,6 +10,5 @@ namespace ControleEstoque.Models
         public int Id { get; set; }
         public string Nome { get; set; }
         public int Preco { get; set; }
-    
     }
 }
--- ControleEstoque/Models/Usuario.cs
+++ ControleEstoque/Models/Usuario.cs
@@ -7,7 +7,6 @@ namespace ControleEstoque.Models
 {
     public class Usuario
     {
-
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
